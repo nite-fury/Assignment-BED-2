0ab78e399623d782e2245f5705ffd0080378650a
Nite-Fury <lennon_su@outlook.com>
2023-07-06 12:50:57 +0800

    first change

---
+++ Assignment-BED-2-main/assignment-BED-2/Public/login.js
@@ -0,0 +1,33 @@
+  $(document).ready(function () {
+    $('#Login').click(function (){
+      let data = {
+        email:$('#email').val(),
+        password:$('#password').val()
+      }
+      if ($('#email').val().length === 0 || $('#password').val().length === 0){
+        $('#msg').text('Field is empty');
+            return;
+      }
+      $.ajax({
+        url: 'http://localhost:8081/user/login',
+        type: 'POST',
+        data: JSON.stringify(data),
+        contentType: 'application/json',
+        dataType: 'json',
+        success:(data, status, xhr) => {
+          if (!data) {
+            $('#msg').text('Credentials are incorrect');
+            return;
+          }
+          localStorage.setItem('token', data.token);
+          localStorage.setItem('userData', data.UserData);
+          location.assign('http://localhost:3001/home.html');
+        },
+        error: (xhr, status, err) => {
+          console.log(err);
+          $('#msg').text('Credentials are incorrect');
+        }
+      })
+      return false;
+    })
+  });
