619181cf1f2c4063c20429bc7d852e7357cf321a
Nite-Fury <lennon_su@outlook.com>
2023-07-24 14:45:20 +0800

    working priceval in jquery

--- Assignment-BED-2-main/assignment-BED-2/FrontEnd/Public/js/add game.js
+++ Assignment-BED-2-main/assignment-BED-2/FrontEnd/Public/js/add game.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
                 console.log(data[cat])
                 tmphtml += `
                 <div class="form-check mb-1">
-                <input class="form-check-input" type="checkbox" form="add_game" value=${data[cat].catid} id=${data[cat].catid}>
+                <input class="form-check-input myCheckbox" type="checkbox" form="add_game" value=${data[cat].catid} id="cat_id${data[cat].catid}">
                 <label class="form-check-label" for=${data[cat].catid}>
                 ${data[cat].catname}
                 </label>
@@ -31,8 +31,8 @@ $(document).ready(function () {
                         console.log(data[plat])
                         tmphtml += `
                         <div class="form-check mb-1">
-                        <input class="form-check-input" type="checkbox" form="add_game" value="${data[plat].platformid}" id="id_${data[plat].platformid}">
-                        <label class="form-check-label" for="${data[plat].platformid}" value="${data[plat].platformid}">
+                        <input class="form-check-input myCheckbox" type="checkbox" form="add_game" value=${data[plat].platformid} id="platid_${data[plat].platformid}">
+                        <label class="form-check-label" for="${data[plat].platformid}" value=platid${data[plat].platformid}>
                         ${data[plat].platform_name}
                         </label>
                         <main id="price_${data[plat].platformid}">
@@ -43,15 +43,15 @@ $(document).ready(function () {
                     }
                     $('#platform').append(tmphtml)
                     platid.forEach((id)=> {
-                        console.log("#id_"+id)
-                        $("#id_"+id).change(()=>{
-                            if ($("#id_"+id).is(":checked")){
+                        console.log("#platid_"+id)
+                        $("#platid_"+id).change(()=>{
+                            if ($("#platid_"+id).is(":checked")){
                                 $('#price_'+id).empty()
                                 $('#price_'+id).append(`
-                                <input type="number" id="price${id}" form="add_game" class="form-control form-control-sm bg-light text-dark" placeholder="Price"/>
+                                <input type="number" class="priceval form-control form-control-sm bg-light text-dark"id="price${id}" form="add_game" class="form-control form-control-sm bg-light text-dark" placeholder="Price"/>
                                 `)
                             }
-                            else if (!$("#id_"+id).is(":checked")){
+                            else if (!$("#platid_"+id).is(":checked")){
                                 $('#price_'+id).empty()
                             }
                         })
@@ -68,50 +68,45 @@ $(document).ready(function () {
             console.log(err);
             console.log("Failed to pull cat data!");
         }
-    })
+    });
+    
     $("#add_game").submit((e)=>{
         e.preventDefault()
-        const data = Object.fromEntries(new FormData(e.target).entries());
-        console.log(data)
-        $("input:checked").each((data)=>{
-                console.log(data)
-                console.log("checkbox")
-            }
-        )
-        $(":input[type='number']").each((index, element) => {
-            console.log(element.value);
-            console.log("price box");
+        console.log($('#title').val())
+        console.log($('#desc').val())
+        console.log($('#year').val())
+        $('.myCheckbox:checked').each(function(data) {
+            var checkboxId = $(this).attr('id');    
+            console.log(checkboxId);
+          });
+          
+        $(".priceval").each(function() {
+            var checkboxId = $(this).attr('id');    
+            console.log(checkboxId +"|"+$("#"+checkboxId).val());
         });
     
         return false
     }) 
-    // $("#add_game").submit((e) => {
-        
-    //     let loginForm = $(this.serializeArray())
-    //     console.log(loginForm)
-        
-        
-        
-    //     // $.ajax({    
-    //     //     url: 'http://localhost:8081/game',
-    //     //     type: 'POST',
-    //     //     data: JSON.stringify(data),
-    //     //     contentType: 'application/json',
-    //     //     dataType: 'json',
-    //     //     success: (data,status, xhr) => {
-    //     //         if (!data) {
-    //     //             $('#msg').text("Failed to submit")
-    //     //             return;
-    //     //         }
-    //     //         location.reload()
-    //     //         $('#msg').text("Data has been submitted Successfully!")
-    //     //     },
-    //     //     error: (xhr, status, err) => {
-    //     //         console.log(err);
-    //     //         $('#msg').text("Failed to submit")
-    //     //     }
-    //     // })
-    //     e.preventDefault();
-    //     return false
-    // })
+    $("#add_game").submit((e) => {
+        e.preventDefault()        
+        $.ajax({    
+            url: 'http://localhost:8081/game',
+            type: 'POST',
+            data: JSON.stringify(data),
+            contentType: 'application/json',
+            dataType: 'json',
+            success: (data,status, xhr) => {
+                if (!data) {
+                    $('#msg').text("Failed to submit")
+                    return;
+                }
+                location.reload()
+                $('#msg').text("Data has been submitted Successfully!")
+            },
+            error: (xhr, status, err) => {
+                console.log(err);
+                $('#msg').text("Failed to submit")
+            }
+        })
+    })
 })
